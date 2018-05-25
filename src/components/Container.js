45aa3387267a86e18a53ba97bae1025e47de4606
zhaoshengqi <zhaoshengqi467@pingan.com.cn>
2018-05-25 16:32:36 +0800

    [解决了input输入框的限制输入只能为数字]

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,8 +8,8 @@ export default class Container extends React.Component {
 		return (
 			<div className = 'container'>
 				<HeaderItems/>
-				<InputItems text="请输入人数"/>
-				<InputItems text="请输入金额"/>
+				<InputItems text="请输入人数" warning="人数必须为整数"/>
+				<InputItems text="请输入金额" warning="钱数必须是数字"/>
 				<ButtonItems text="发红包"/>
 			</div>
 		)
--- src/components/InputItems.js
+++ src/components/InputItems.js
@@ -4,20 +4,45 @@ import '../styles/InputItems.scss'
 export default class InputItems extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			value: "",
+			errorInfo: 'hidden'
+		};
 	}
-	keyLimit = (value) => {
-		console.log(value);
+	keyLimit = (e) => {
+		const keycode = e.keyCode;
+		if (keycode >= 48 && keycode <= 57) {
+			const keychar = String.fromCharCode(keycode);
+			const oldValue = this.state.value;
+			const newValue = oldValue + keychar;
+			this.setState({
+				value: newValue
+			});
+			this.setState({
+				errorInfo: 'hidden'
+			});
+		} else if (keycode == 8) { //keycode=8为删除键
+			const oldValue = this.state.value;
+			const newValue = oldValue.substr(0, oldValue.length - 1);
+			this.setState({
+				value: newValue
+			});
+		} else if (keycode != 13) {
+			this.setState({
+				errorInfo: 'visible'
+			});
+		}
 	}
 	render() {
 		const {
-			text
+			text,
+			warning,
 		} = this.props;
 		return (
 			<div className='input-component'>
 				<label className='input-info'>{text}</label>
-				<input type="text" className='input-text'/>
-				<div className="input-warning">warning</div>
+				<input type="text" className='input-text' value={this.state.value} onKeyDown={(event)=>this.keyLimit(event)} />
+				<div className="input-warning" style={{visibility:this.state.errorInfo}}>{warning}</div>
 			</div>
 		)
 	}
